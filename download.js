431ff1c2ae4b8a1282f1859bc98f56cd0dc0a5f4
Abi Hafshin <abi@hafs.in>
2022-03-01 17:45:37 +0700

    download prebuilt exe using node

--- download.js
+++ download.js
@@ -1,7 +1,12 @@
 // @ts-check
 
 import { execSync } from "child_process";
-import { chmodSync, existsSync, readFileSync, rmSync, symlinkSync } from "fs";
+import { once } from "events";
+import { createWriteStream, existsSync, readFileSync, rmSync, symlinkSync, writeFileSync } from "fs";
+import { chmod } from "fs/promises";
+import http from "http";
+import https from "https";
+import { URL } from "url";
 import { exefile } from "./exe.js";
 
 async function main() {
@@ -32,13 +37,13 @@ async function main() {
 
     const url = `https://github.com/abihf/depgraph/releases/download/v${version}/${artifactName}`;
     console.log(`Downloading ${url}`);
-    exec(`wget -O "${exefile}" "${url}"`);
-    chmodSync(exefile, "755");
+    await downloadExe(url);
   } catch (e) {
     console.error("Download error", e);
     console.log("Trying to build from source");
-    exec(`sed -i.bak '/^version = /c\\version = \"${version}\"' Cargo.toml`);
+    replaceCargoVersion(version);
     exec("cargo build --release && ln -sf target/release/depgraph depgraph");
+    console.log("Build successful");
   }
 }
 
@@ -82,8 +87,47 @@ function trySymlink(version) {
 
 /**
  *
+ * @param {string} urlStr
+ */
+async function downloadExe(urlStr) {
+  let url = new URL(urlStr);
+  let maxRedirect = 5;
+
+  while (maxRedirect > 0) {
+    const res = await new Promise((resolve, reject) =>
+      (url.protocol === "https" ? https : http).get(url, resolve).on("error", reject)
+    );
+    const status = res.statusCode;
+    if (status === 200) {
+      const file = createWriteStream(exefile);
+      const pipe = res.pipe(file);
+      await once(pipe, "close");
+      await chmod(exefile, "755");
+      return;
+    } else if ([301, 302, 303, 307, 308].includes(status)) {
+      url = new URL(res.headers.location, url);
+      maxRedirect--;
+    } else {
+      throw new Error(`unexecpected code ${status} from ${url.toString()}`);
+    }
+  }
+  throw new Error(`reach max redirect for ${url.toString()}`);
+}
+
+/**
+ *
+ * @param {string} version
+ */
+function replaceCargoVersion(version) {
+  let content = readFileSync("Cargo.toml", "utf-8");
+  content = content.replace(/^version = .*$/, `version = "${version}"`);
+  writeFileSync("Cargo.toml", content, "utf-8");
+}
+
+/**
+ *
  * @param {string} cmd
  */
-function exec(cmd) {
-  return execSync(cmd, { stdio: "inherit" });
+ function exec(cmd) {
+  return execSync(cmd, { stdio: ["ignore", "inherit", "inherit"] });
 }
