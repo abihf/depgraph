36c3691ee06724a79b63b719469df73640a7f634
Abi Hafshin Alfarouq <abi@hafs.in>
2022-03-18 03:28:55 +0700

    delete

--- test.js
+++
@@ -1,9 +0,0 @@
-import { analyze } from './index.js';
-
-async function main() {
-  for await (const [file, dep] of analyze(['/home/abi/tvlk/www/packages/core/index.ts'])) {
-    console.log(file, dep)
-  }
-}
-
-main()
