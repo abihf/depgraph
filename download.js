4a264f5aa765dff48f287667f5e60b56ddef1773
Abi Hafshin <abi@hafs.in>
2022-12-30 23:24:47 +0700

    use musl for linux build

--- download.js
+++ download.js
@@ -22,8 +22,8 @@ async function main() {
   try {
     /** @type {string} */
     const artifactName = {
-      "linux-x64": "depgraph-x86_64-unknown-linux-gnu",
-      "linux-arm64": "depgraph-aarch64-unknown-linux-gnu",
+      "linux-x64": "depgraph-x86_64-unknown-linux",
+      "linux-arm64": "depgraph-aarch64-unknown-linux",
       "darwin-x64": "depgraph-x86_64-apple-darwin",
       "darwin-arm64": "depgraph-aarch64-apple-darwin",
     }[`${process.platform}-${process.arch}`];
